e795f3e74134d46c5cb6aeeefe3c3151aaf736e5
Briyith sanchez <briyithd.sancheza@uqvirtual.edu.co>
2026-04-23 08:12:28 -0500

    se crea clase principal con CRUD evento y  persona

--- Modelo/PF-Events/src/main/java/co/edu/uniquindio/poo/model/DetalleEvento.java
+++ Modelo/PF-Events/src/main/java/co/edu/uniquindio/poo/model/DetalleEvento.java
@@ -1,10 +1,52 @@
 public class DetalleEvento{
     private Evento ownedByEvento;
     private List<Politica> listaPoliticas;
+    private Sting descripcion;
+    private String lugar;
+    private LocalDate fecha;
+    private String aforo;
 
-    public DetalleEvento(List<Politica> listaPoliticas, Evento ownedByEvento) {
+
+    public DetalleEvento(String aforo, Sting descripcion, LocalDate fecha, List<Politica> listaPoliticas,
+                         String lugar, Evento ownedByEvento) {
+        this.aforo = aforo;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
         this.listaPoliticas = listaPoliticas;
+        this.lugar = lugar;
         this.ownedByEvento = ownedByEvento;
+    }
+
+    public String getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(String aforo) {
+        this.aforo = aforo;
+    }
+
+    public Sting getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(Sting descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
     }
 
     public List<Politica> getListaPoliticas() {
